721e75b0e6254eefeb11635fca6448fd8f4ca789
ln <lina771@qq.com>
2019-08-22 10:54:39 +0800

    新建项目

---
+++ rice_demo/src/main/java/com/rice/demo/RiceDemoApplication.java
@@ -0,0 +1,17 @@
+package com.rice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @Description: 启动类
+ * @Author: ln
+ * @Date: 2019/8/22 10:52
+ **/
+@SpringBootApplication
+public class RiceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiceDemoApplication.class, args);
+    }
+
+}
